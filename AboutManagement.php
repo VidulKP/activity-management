92261a3b3b552cd9ac5a9cbefeb25fe7e01c9726
VidulKP <vidul.kp@gmail.com>
2017-11-22 22:57:22 +0530

    Update AboutManagement.php

--- AboutManagement.php
+++ AboutManagement.php
@@ -7,20 +7,20 @@
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.min.css"/>
         <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
         <link rel="stylesheet" type="text/css" href="css/home.css" />
-        <title>This project is contributed to my College. And group project's made by Suman Arora, Sudha Arora, and Sonali Changal.</title>
+        <title>This project was made by Piyush Panicker,Vidul K.P and Muhammad Shibin.</title>
     </head>
     <body>
         <div class="panel panel-default">
             <div class="panel-heading"><h1 style="text-align: center;"><span class="glyphicon glyphicon-user"></span> About Project!</h1></div>
   			<div class="panel-body">
 			<div class="container">
-				<p style="text-align:center;">Group project's contributed to my college.</p>
+				<p style="text-align:center;">RDBMS Project-CSAMS</p>
                         </div><br><br>
 
-                        <h1><strong>Project title: </strong>College Management System</h1>
-                        <h1><strong>Made by: </strong>Suman Arora, Sudha Arora, and Sonali Changal.</h1>
-                        <h1><strong>College: </strong>Government Polytechnic college.</h1>
-                        <h1><strong>Branch: </strong>Computer science(Final year student).</h1>
+                        <h1><strong>Project title: </strong>CUSAT Student Management System</h1>
+                        <h1><strong>Made by: </strong>Piyush Panicker,Vidul K.P and Muhammad Shibin</h1>
+                        <h1><strong>Branch: </strong>Information Technology</h1>
+                        <h1><strong>College: </strong>School Of Enginnering,CUSAT</h1>
                         </div>
         </div>
     </body>
