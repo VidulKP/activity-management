2406fb3fca4e8ec6a9cb1218cc4f57d8e4ef23b4
VidulKP <vidul.kp@gmail.com>
2017-11-22 22:41:05 +0530

    Update home.php

--- home.php
+++ home.php
@@ -3,27 +3,26 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="css/home.css"/>
-<title>College management system made by Rajendra Arora..</title>
+<title>CSAMS</title>
 </head>
 
 <body>
 	<div class="panel panel-default">
-  		<div class="panel-heading"><h1>College Management System</h1></div>
+  		<div class="panel-heading"><h1>CUSAT Student Activity Management System</h1></div>
                 <div class="panel-body"><br><br>
         <div>
-            <img src="images/rtupic.jpg" width="855" style="text-align: center;" height="308" onmousedown="return false;"  />
+            <img src="images/cusat.jpg" width="855" style="text-align: center;" height="308" onmousedown="return false;"  />
         </div><br><br>
         <div class="middle_text">
-            <p><strong>Rajasthan Technical University (RTU) is an affiliating university located in Kota in the state of Rajasthan, India. The university was established in 2006.</strong></p>
+            <p><strong>Cochin University of Science and Technology (CUSAT) is a government-owned autonomous science and technology university in Cochin, Kerala, India. It was founded in 1971.</strong></p>
         </div>
-                
+
         <div class="text_para_home">
-            <p>RTU is an affiliating university. Its affiliated colleges offer the degrees of Bachelor of Technology (B.Tech), Master of Technology (M.Tech), Master of Business Administration (MBA), Master of Computer Applications (MCA) and Bachelor of Hotel Management and Catering Technology (BHMCT).
-As of April 2011, RTU affiliates 202 colleges. B.Tech degrees are offered at nine government aided institutes and 109 are private ones. MBA degrees are offered at seven government aided institutes and 122 are private ones. MCA degrees are offered in at seven government aided institutes and 25 are private ones. M.Tech is offered in 24 colleges and BHMCT in four colleges. In addition, two colleges offer Bachelor of Architecture (B.Arch) degrees. Rajasthan University Publishes Courses Results Semester wise.</p>         
-        </div>   
+            <p>The School of Engineering is a college under Cochin University of Science and Technology, in Kochi (Cochin), Kerala, India. Established in 1979 for offering part-time M.Tech programmes.The school was the first in the country to introduce Information Technology as an engineering stream and is one among very few colleges in the country with a B. Tech course in Safety and Fire Engineering. The school is a Research Centre and major Consultancy Centre. A number of research projects of national importance have been sanctioned to the school by agencies like DRDO, ISRO, DST, AICTE, UGC, Coir Board, and the Coconut Development Board.</p>
+        </div>
                 </div>
         </div>
-    
-    
+
+
 </body>
 </html>
